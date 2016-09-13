ba891b0136054d9474d0c24d2ee50ec28b1b0ef0
Pawel <Pawel.Maciulewski@gmail.com>
2016-09-13 14:32:00 -0600

    enable cors

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,6 @@ var db = require('monk')(process.env.MONGODB_URI || 'mongodb://localhost:27017/d
 var companies = db.get('companies');
 var sciencebase = db.get('sciencebase');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
