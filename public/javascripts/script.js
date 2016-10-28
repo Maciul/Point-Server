2f7bf022f66c9e2ba8d258a249f2118bd3dfbf81
Pawel <Pawel.Maciulewski@gmail.com>
2016-10-28 10:35:52 -0400

    delete all views files

--- public/javascripts/script.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-
-$('#demo').click(function(){
-  $('#loading').show().delay(5300).fadeOut();
-  setTimeout(function(){
-    $('#upload').show().delay(5000).fadeOut();
-  }, 6200);
-});
-});
